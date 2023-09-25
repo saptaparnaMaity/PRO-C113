907e3748cd6f963b4bc3c5fe49f33d782ebcce94
saptaparnaMaity <75085120+saptaparnaMaity@users.noreply.github.com>
2023-09-25 14:52:45 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,64 @@
+from os import stat
+import pandas as pd
+import statistics
+import plotly.express as px
+import csv
+import plotly.graph_objects as go
+
+#Plotting the graph
+df = pd.read_csv("savings_data_final.csv")
+fig = px.scatter(df, y="quant_saved", color="rem_any")
+fig.show()
+
+with open('savings_data_final.csv', newline="") as f:
+  reader = csv.reader(f)
+  savings_data = list(reader)
+
+savings_data.pop(0)
+
+#Finding total number of people and number of people who were reminded
+total_entries = len(savings_data)
+total_people_given_reminder = 0
+for data in savings_data:
+  if int(data[3]) == 1:
+    total_people_given_reminder += 1
+
+
+
+fig = go.Figure(go.Bar(x=["Reminded", "Not Reminded"], y=[total_people_given_reminder, (total_entries - total_people_given_reminder)]))
+
+fig.show()
+
+
+#Mean, median and mode of savings
+all_savings = []
+
+for data in savings_data:
+    all_savings.append(float(data[0]))
+
+    
+print(f"Mean of Savings - {statistics.mean(all_savings)}")
+print(f"Median of Savings - {statistics.median(all_savings)}")
+print(f"Mode of Savings - {statistics.mode(all_savings)}")
+
+
+
+#Mean, median and mode of savings
+reminded_savings = []
+not_reminded_savings = []
+for data in savings_data:
+  if int(data[3]) == 1:
+    reminded_savings.append(float(data[0]))
+  else:
+    not_reminded_savings.append(float(data[0]))
+
+print("Results for people who were reminded to save")
+print(f"Mean of savings - {statistics.mean(reminded_savings)}")
+print(f"Median of savings - {statistics.median(reminded_savings)}")
+print(f"Mode of savings - {statistics.mode(reminded_savings)}")
+#To add new lines
+print("\n\n")
+print("Results for people who were not reminded to save")
+print(f"Mean of savings - {statistics.mean(not_reminded_savings)}")
+print(f"Median of savings - {statistics.median(not_reminded_savings)}")
+print(f"Mode of savings - {statistics.mode(not_reminded_savings)}")
